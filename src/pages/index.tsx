ab2525de6ed68f1a4173da393129e022673673f3
Metoděj Janota <metodejjanota@gmail.com>
2025-03-11 15:12:10 +0100

    next.js and capacitor

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<div>
+			<h1>Travel Ready</h1>
+			<p>
+				Welcome to Travel Ready! This is a travel app that helps you plan your
+				trips.
+			</p>
+		</div>
+	);
+}

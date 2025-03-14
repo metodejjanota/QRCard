cb10b228f2582e0c6a88b64f7b560bd79a5959d4
Metoděj Janota <metodejjanota@gmail.com>
2025-03-14 11:19:24 +0100

    Card and styles

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { BellIcon } from "lucide-react";
-import { useRouter } from "next/router";
 
 const NavTop = () => {
 	return (
-		<div className="w-full h-20 fixed bg-amber-500 top-0 left-0 items-center">
-			<div className="container ">
+		<div className="w-full h-20 fixed bg-accent top-0 left-0 items-center">
+			<div className="max-w-xs mx-auto h-full">
 				<div className="w-full h-full flex justify-between items-center">
 					<div>
-						<h1 className="text-2xl font-black"></h1>
-						<p className="text-sm">
-							This is a travel app that helps you plan your trips.
-						</p>
+						<h1 className="text-2xl font-black">QRCard</h1>
 					</div>
 
 					<div>
-						<BellIcon size={24} />
+						<BellIcon size={24} strokeWidth={3} />
 					</div>
 				</div>
 			</div>
@@ -24,15 +20,11 @@ const NavTop = () => {
 };
 
 const NavBottom = () => {
-	const router = useRouter();
 	return (
 		<div className="fixed left-0 bottom-0 w-full h-30 flex justify-around items-center">
 			<div className="w-full h-full flex justify-around items-center">
-				<button
-					className="btn btn-outline btn-lg"
-					onClick={() => router.push("/auth/login")}
-				>
-					nevim
+				<button className="btn btn-outline btn-lg">
+					share my profile link
 				</button>
 			</div>
 		</div>
--- src/pages/protected/dashboard.tsx
+++ src/pages/protected/dashboard.tsx
@@ -5,8 +5,30 @@ import { createClient } from "@/lib/supabase/server-props";
 
 export default function PrivatePage({ user }: { user: User }) {
 	return (
-		<div>
-			<h1>Hello, {user.email || "user"}!</h1>);
+		<div className="w-full h-full flex flex-col gap-2">
+			<div className="w-full h-full flex justify-center items-center aspect-square bg-accent rounded-2xl"></div>
+			<div className="w-full h-full flex flex-col gap-12 text-center">
+				<div className="flex flex-col gap-2">
+					<h1 className="text-2xl font-bold">{user.email || "user"}</h1>
+					<p className="text-lg opacity-70">
+						Welcome to your private page. You are logged in.
+					</p>
+				</div>
+				<p className="text-sm opacity-70">
+					If you want to edit your business card,
+					<span
+						onClick={() => {
+							if (typeof window !== "undefined") {
+								window.location.pathname = "/protected/edit";
+							}
+						}}
+						className="text-accent cursor-pointer"
+						style={{ textDecoration: "underline" }}
+					>
+						click here
+					</span>
+				</p>
+			</div>
 		</div>
 	);
 }

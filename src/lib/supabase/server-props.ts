f3ce45d164585f3344f182debac2d5e78d056316
Maxmilián Růžička <holsek12@gmail.com>
2025-03-30 19:37:52 +0200

    bookmarks

--- src/lib/supabase/server-props.ts
+++ src/lib/supabase/server-props.ts
@@ -1,43 +1,43 @@
 import { NextPageContext, type GetServerSidePropsContext } from "next";
 import {
-	createServerClient,
-	createBrowserClient,
-	serializeCookieHeader,
+    createServerClient,
+    createBrowserClient,
+    serializeCookieHeader,
 } from "@supabase/ssr";
 
-export function createClient(context: NextPageContext) {
-	if (typeof window === "undefined" && context && context.req && context.res) {
-		const { req, res } = context as unknown as GetServerSidePropsContext;
+export function createClient(context?: NextPageContext) {
+    if (typeof window === "undefined" && context && context.req && context.res) {
+        const { req, res } = context as unknown as GetServerSidePropsContext;
 
-		return createServerClient(
-			process.env.NEXT_PUBLIC_SUPABASE_URL!,
-			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-			{
-				cookies: {
-					getAll() {
-						if (!req.cookies) return [];
-						return Object.keys(req.cookies).map(name => ({
-							name,
-							value: req.cookies[name] || "",
-						}));
-					},
-					setAll(cookiesToSet) {
-						if (res && res.setHeader) {
-							res.setHeader(
-								"Set-Cookie",
-								cookiesToSet.map(({ name, value, options }) =>
-									serializeCookieHeader(name, value, options)
-								)
-							);
-						}
-					},
-				},
-			}
-		);
-	} else {
-		return createBrowserClient(
-			process.env.NEXT_PUBLIC_SUPABASE_URL!,
-			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-		);
-	}
+        return createServerClient(
+            process.env.NEXT_PUBLIC_SUPABASE_URL!,
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+            {
+                cookies: {
+                    getAll() {
+                        if (!req.cookies) return [];
+                        return Object.keys(req.cookies).map(name => ({
+                            name,
+                            value: req.cookies[name] || "",
+                        }));
+                    },
+                    setAll(cookiesToSet) {
+                        if (res && res.setHeader) {
+                            res.setHeader(
+                                "Set-Cookie",
+                                cookiesToSet.map(({ name, value, options }) =>
+                                    serializeCookieHeader(name, value, options)
+                                )
+                            );
+                        }
+                    },
+                },
+            }
+        );
+    } else {
+        return createBrowserClient(
+            process.env.NEXT_PUBLIC_SUPABASE_URL!,
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+        );
+    }
 }
--- src/pages/protected/bookmarks.tsx
+++ src/pages/protected/bookmarks.tsx
@@ -1,102 +1,102 @@
-const testData = [
-	{
-		_id: "1",
-		companyLogo: "/images/company1.png",
-		copmanyName: "TechCorp",
-		copmanyPosition: "Software Engineer",
-		description: "Innovative tech solutions provider.",
-		firstName: "John",
-		lastName: "Doe",
-		website: "https://techcorp.com",
-		phone: "123-456-7890",
-		email: "john.doe@techcorp.com",
-	},
-	{
-		_id: "2",
-		companyLogo: "/images/company2.png",
-		copmanyName: "HealthPlus",
-		copmanyPosition: "Product Manager",
-		description: "Healthcare services and products.",
-		firstName: "Jane",
-		lastName: "Smith",
-		website: "https://healthplus.com",
-		phone: "987-654-3210",
-		email: "jane.smith@healthplus.com",
-	},
-	{
-		_id: "3",
-		companyLogo: "/images/company3.png",
-		copmanyName: "EduWorld",
-		description: "Educational resources for all ages.",
-		firstName: "Alice",
-		lastName: "Johnson",
-		website: "https://eduworld.com",
-		email: "alice.johnson@eduworld.com",
-	},
-];
-
 import { ICard } from "@/lib/types/card";
 import { Trash2Icon } from "lucide-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { createClient } from "@/lib/supabase/server-props";
 
 const Bookmarks = () => {
-	const router = useRouter();
-	const [bookmarksList, setBookmarksList] = useState<ICard[]>(testData);
+    const router = useRouter();
+    const [bookmarksList, setBookmarksList] = useState<ICard[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
-	const deleteBookmark = (id: string) => {
-		setBookmarksList(prev => prev.filter(bookmark => bookmark._id !== id));
-		// + odstrani bookmark i z db
-	};
+    const supabase = createClient();
 
-	const openBookmark = (id: string) => {
-		const bookmark = bookmarksList.find(b => b._id === id);
-		if (bookmark) {
-			router.push(`/card/${id}`);
-		}
-	};
+    const fetchBookmarks = async () => {
+        setLoading(true);
+        const { data, error } = await supabase
+            .from("bookmarks")
+            .select("*");
 
-	return (
-		<div>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-				{bookmarksList.map(bookmark => (
-					<div
-						key={bookmark._id}
-						className="card bg-base-100 card-sm shadow-sm hover:shadow-lg transition-all duration-300 ease-in-out p-4 rounded-lg flex flex-row gap-4 cursor-pointer"
-						onClick={() => openBookmark(bookmark._id)}
-					>
-						<div className="absolute top-2 right-2 cursor-pointer m-2">
-							<Trash2Icon
-								size={20}
-								strokeWidth={2}
-								className="opacity-70 cursor-pointer"
-								onClick={() => deleteBookmark(bookmark._id)}
-							/>
-						</div>
-						<Image
-							src={bookmark.companyLogo || "/images/default.png"}
-							alt={bookmark.copmanyName}
-							width={100}
-							height={100}
-							className="rounded-lg"
-						/>
-						<div className="flex flex-col gap-2 mt-4  items-start">
-							<h2>{bookmark.copmanyName}</h2>
-							<p>{bookmark.description}</p>
-							<a
-								href={bookmark.website}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								Visit Website
-							</a>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+        if (error) {
+            console.error("Error fetching bookmarks:", error);
+        } else {
+            setBookmarksList(data || []);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchBookmarks();
+    }, []);
+
+    const deleteBookmark = async (id: string) => {
+        const { error } = await supabase
+            .from("bookmarks")
+            .delete()
+            .eq("id", id);
+
+        if (error) {
+            console.error("Error deleting bookmark:", error);
+        } else {
+            setBookmarksList(prev => prev.filter(bookmark => bookmark._id !== id));
+        }
+    };
+
+    const openBookmark = (id: string) => {
+        const bookmark = bookmarksList.find(b => b._id === id);
+        if (bookmark) {
+            router.push(`/card/${id}`);
+        }
+    };
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {bookmarksList.map(bookmark => (
+                    <div
+                        key={bookmark._id}
+                        className="card bg-base-100 card-sm shadow-sm hover:shadow-lg transition-all duration-300 ease-in-out p-4 rounded-lg flex flex-row gap-4 cursor-pointer"
+                        onClick={() => openBookmark(bookmark._id)}
+                    >
+                        <div className="absolute top-2 right-2 cursor-pointer m-2">
+                            <Trash2Icon
+                                size={20}
+                                strokeWidth={2}
+                                className="opacity-70 cursor-pointer"
+                                onClick={e => {
+                                    e.stopPropagation();
+                                    deleteBookmark(bookmark._id);
+                                }}
+                            />
+                        </div>
+                        <Image
+                            src={bookmark.companyLogo || "/images/default.png"}
+                            alt={bookmark.copmanyName}
+                            width={100}
+                            height={100}
+                            className="rounded-lg"
+                        />
+                        <div className="flex flex-col gap-2 mt-4 items-start">
+                            <h2>{bookmark.copmanyName}</h2>
+                            <p>{bookmark.description}</p>
+                            <a
+                                href={bookmark.website}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Visit Website
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Bookmarks;
